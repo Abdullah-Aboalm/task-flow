ec6cc1186bd2245878cf49c4f75c5ec9a20ed285
Abdullah <abdullahaboalm2004@gmail.com>
2025-03-21 18:00:23 +0200

    Fix logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,14 @@
 import { themeStore } from "../store";
 import { PiMoonLight, PiSunLight } from "react-icons/pi";
+import darkLogo from "../assets/darkLogo.png";
+import lightLogo from "../assets/LightLogo.png";
 
 const Navbar = () => {
   const { theme, toggleTheme } = themeStore();
   return (
     <header className="p-4 shadow bg-white text-dark-gray dark:bg-dark-gray dark:text-white">
       <nav className="flex bg-transparent items-center justify-between">
-        <img
-          src={`/src/assets/${
-            theme === "dark" ? "lightLogo.png" : "darkLogo.png"
-          }`}
-          alt="logo"
-        />
+        <img src={theme === "dark" ? lightLogo : darkLogo} alt="logo" />
         <button onClick={toggleTheme}>
           {theme === "dark" ? (
             <PiSunLight size={20} />
